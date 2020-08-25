9b66ff9d9bff15e55829f33a14b7e64842627729
bandyp <nd201175@gmail.com>
2020-08-25 09:59:26 +0000

    added jquery to submit correctly

--- index.php
+++ index.php
@@ -18,12 +18,18 @@
     <form>
         <div class="form-group">
             <label for="exampleFormControlInput1">Email address</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            <input type="email" class="form-control" id="email" placeholder="Enter your email address">
+            <small class="text-muted">We'll never share your email with anyone else.</small>
         </div>
         <div class="form-group">
-            <label for="exampleFormControlTextarea1">Example textarea</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <label for="subject">Subject</label>
+            <input type="text" class="form-control" id="subject">
         </div>
+        <div class="form-group">
+            <label for="content">How can i help?</label>
+            <textarea class="form-control" id="content" rows="3"></textarea>
+        </div>
+        <button type="submit" id="submit" class="btn btn-primary">Submit</button>
     </form>
     </div>
     <!-- Optional JavaScript -->
@@ -31,5 +37,33 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
+    
+    <script type="text/javascript">
+
+        $("form").submit(function (e) {
+            e.preventDefault(); // this will prevent from submitting the form.
+
+            var error = "";
+
+            if ($("#subject").val() == "") {
+
+                error += "<p>The Subject field is required</p>";
+
+            }
+
+            if ($("#content").val() =="") {
+                
+                error += "<p>The Content field is required</p>";
+
+            }
+
+            if (error != "") {
+                
+                $("#error").html('<div class="alert alert-danger" role="alert"><strong>There were error(s) in your form:</strong>' + error + '</div>);
+
+            }    
+        });
+    </script>
+  
+    </body>
 </html>
